f7a6bc4eff665a63140a20a06499462e1f7f3cb6
Shane McKee <shane.mckee@canonical.com>
2023-08-18 18:46:49 -0600

    Cleanup install script & relocate run script

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A layer on top of PTS automation which simplifies the PTS install + run process 
 
 ## Usage
 Download the appropriate download cache, untar it, and move it to ~/.phoronix-test-suite/download-cache
-$ ./install-deps.sh
-$ ./run.sh
+
+`$ ./install-deps.sh`\
+`$ ./<test suite>/run.sh`
 
--- install-deps.sh
+++ install-deps.sh
@@ -1,10 +1,12 @@
-PTS_DEB_URL=https://phoronix-test-suite.com/releases/repo/pts.debian/files/phoronix-test-suite_10.8.4_all.deb
+PTS_DEB_FILE=phoronix-test-suite_10.8.4_all.deb
+PTS_DEB_URL=https://phoronix-test-suite.com/releases/repo/pts.debian/files/$PTS_DEB_FILE
 
+sudo apt update
 sudo apt install php-cli php-xml build-essential xterm p7zip-full nasm yasm libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libopencv-dev freeglut3-dev libssl-dev openssl build-essential php-cli php-xml build-essential autoconf mesa-utils vulkan-tools unzip apt-file
 
 pip3 install meson
 
 wget $PTS_DEB_URL
-sudo dpkg -i $PTS_DEB_URL
+sudo dpkg -i $PTS_DEB_FILE
 
-cp custom-phoronix-suites/* $HOME/.phoronix-test-suite/test-suites/local/
+cp -r phoronix-custom-suites/* $HOME/.phoronix-test-suite/test-suites/local/
