74b298354385bd66f57e5a23086e996bf4977305
Shane McKee <shane.mckee@canonical.com>
2023-09-01 16:19:43 -0600

    Add the 'short' test suite

---
+++ short/run-v3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/../set_test_env.sh
+
+# make sure we don't re-use tests that were built with different flags
+rm -rf $HOME/.phoronix-test-suite/installed-tests/
+
+export TEST_RESULTS_IDENTIFIER=short-v3-id 
+export TEST_RESULTS_NAME=short-v3-name 
+
+# PTS grabs these flags in pts_test_installer.php
+export CFLAGS='-march=x86-64-v3'
+export CXXFLAGS='-march=x86-64-v3'
+export FFLAGS='-march=x86-64-v3'
+
+export QMAKE_CXXFLAGS += $(CXXFLAGS)
+export QMAKE_CFLAGS += $(CFLAGS)
+
+export TEST_RESULTS_DESCRIPTION='A test suite which avoids Python wheels or prebuilt bins. Builds are optimized for x86-64-v3'
+
+PTS_SILENT_MODE=1 phoronix-test-suite batch-benchmark short
+
+phoronix-test-suite result-file-to-csv short-name
---
+++ short/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+unset CFLAGS
+unset CXXFLAGS
+unset FFLAGS
+
+# make sure we don't re-use tests that were built with different flags
+rm -rf $HOME/.phoronix-test-suite/installed-tests/
+
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/../set_test_env.sh
+
+export TEST_RESULTS_IDENTIFIER=short-id 
+export TEST_RESULTS_NAME=short-name 
+export TEST_RESULTS_DESCRIPTION='A shorter version of a test suite which avoids\
+       	Python wheels or prebuilt bins'
+
+PTS_SILENT_MODE=1 phoronix-test-suite batch-benchmark short
+
+phoronix-test-suite result-file-to-csv short-name
