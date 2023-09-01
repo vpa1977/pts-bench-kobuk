c43a378dc9cf1051cdabfd25c4ca1838d1057705
Shane McKee <shane.mckee@canonical.com>
2023-09-01 15:40:42 -0600

    Add the non-python-optimizable test suite

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,7 @@ PTS_DEB_FILE=phoronix-test-suite_10.8.4_all.deb
 PTS_DEB_URL=https://phoronix-test-suite.com/releases/repo/pts.debian/files/$PTS_DEB_FILE
 
 sudo apt update
-sudo apt install -y php-cli php-xml build-essential xterm p7zip-full nasm yasm libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libopencv-dev freeglut3-dev libssl-dev openssl php-cli php-xml autoconf mesa-utils vulkan-tools unzip apt-file cmake ninja-build meson
+sudo apt install -y php-cli php-xml build-essential xterm p7zip-full nasm yasm libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libopencv-dev freeglut3-dev libssl-dev openssl php-cli php-xml autoconf mesa-utils vulkan-tools unzip apt-file cmake ninja-build meson gawk bison
 
 pip3 install meson
 
@@ -17,6 +17,12 @@ phoronix-test-suite list-available-tests
 
 cp -r phoronix-custom-suites/* $HOME/.phoronix-test-suite/test-suites/local/
 cp ./config/user-config.xml $HOME/.phoronix-test-suite/
+
+# A bit of a hack to fix the gl-vs-vk test
+sudo ln -s /usr/lib/x86_64-linux-gnu/libGLEW.so /usr/lib/x86_64-linux-gnu/libGLEW.so.1.13
+
+# Enables the mixbench test
+sudo ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 /usr/lib/libOpenCL.so
 
 # This checks if a device has a display and installs a dummy display if not
 if [ -z $DISPLAY ]; then
---
+++ non-python-optimizable/run-v3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+source $SCRIPT_DIR/../set_test_env.sh
+
+# make sure we don't re-use tests that were built with different flags
+rm -rf $HOME/.phoronix-test-suite/installed-tests/
+
+export TEST_RESULTS_IDENTIFIER=non-python-optimizable-id 
+export TEST_RESULTS_NAME=non-python-optimizable-name 
+
+# PTS grabs these flags in pts_test_installer.php
+export CFLAGS='-march=x86-64-v3'
+export CXXFLAGS='-march=x86-64-v3'
+export FFLAGS='-march=x86-64-v3'
+
+export QMAKE_CXXFLAGS += $(CXXFLAGS)
+export QMAKE_CFLAGS += $(CFLAGS)
+
+export TEST_RESULTS_DESCRIPTION='A test suite which avoids Python wheels or prebuilt bins. Builds are optimized for x86-64-v3'
+
+PTS_SILENT_MODE=1 phoronix-test-suite batch-benchmark non-python-optimizable
+
+phoronix-test-suite result-file-to-csv non-python-optimizable
