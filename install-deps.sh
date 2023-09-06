26421b2901e4fe5e26b05322bcc4035a68e81456
Shane McKee <shane.mckee@canonical.com>
2023-09-06 17:57:05 -0600

    Add deps for qgears2 and vkfft

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,7 @@ PTS_DEB_FILE=phoronix-test-suite_10.8.4_all.deb
 PTS_DEB_URL=https://phoronix-test-suite.com/releases/repo/pts.debian/files/$PTS_DEB_FILE
 
 sudo apt update
-sudo apt install -y php-cli php-xml build-essential xterm p7zip-full nasm yasm libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libopencv-dev freeglut3-dev libssl-dev openssl php-cli php-xml autoconf mesa-utils vulkan-tools unzip apt-file cmake ninja-build meson gawk bison
+sudo apt install -y php-cli php-xml build-essential xterm p7zip-full nasm yasm libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libopencv-dev freeglut3-dev libssl-dev openssl php-cli php-xml autoconf mesa-utils vulkan-tools unzip apt-file cmake ninja-build meson gawk bison qtbase5-dev qt5-qmake libfftw3-dev fftw-dev
 
 pip3 install meson
 
