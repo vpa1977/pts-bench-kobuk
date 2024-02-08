8308d1bc8c0292c99a6042811d3293bc05f513be
Vladimir Petko <vladimir.petko@canonical.com>
2024-02-08 18:16:43 +1300

    test env - do not check checksums

--- set_test_env.sh
+++ set_test_env.sh
@@ -5,3 +5,4 @@ export WATCHDOG_SENSOR=cpu.temp,gpu.temp,sys.temp,hdd.temp
 export WATCHDOG_SENSOR_THRESHOLD=85
 # If it's not returning to lower temps, continue after this many min
 export WATCHDOG_MAXIMUM_WAIT=3
+export NO_FILE_HASH_CHECKS=1
