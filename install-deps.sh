7465b1235e7779a551c4e0c1665ca3653a95b80c
Vladimir Petko <vladimir.petko@canonical.com>
2024-02-09 15:20:04 +1300

    add lightdm

--- install-deps.sh
+++ install-deps.sh
@@ -31,4 +31,5 @@ sudo ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 /usr/lib/libOpenCL.so
 
 # system-pts dependencies
 sudo apt install libtiff-tools libjpeg-progs libxml2 libxml2-utils blender clpeak pbzip2 cryptsetup gegl gmic gnuradio hugin inkscape \
-    rawtherapee libreoffice tesseract-ocr sqlite3 librsvg2-bin octave openscad ocrmypdf nginx mpv gimp
+    rawtherapee libreoffice tesseract-ocr sqlite3 librsvg2-bin octave openscad ocrmypdf nginx mpv gimp \
+    lightdm
