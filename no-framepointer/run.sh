bd7e27418f55733131209aa6448a0719fa633e11
Vladimir Petko <vladimir.petko@canonical.com>
2024-02-10 08:49:52 +1300

    nofp

--- no-framepointer/run.sh
+++ no-framepointer/run.sh
@@ -10,7 +10,7 @@ source $SCRIPT_DIR/../set_test_env.sh
 PTS_BIN=$SCRIPT_DIR/../pts-source/phoronix-test-suite
 
 # make sure we don't re-use tests that were built with different flags
-rm -rf $HOME/.phoronix-test-suite/installed-tests/
+#rm -rf $HOME/.phoronix-test-suite/installed-tests/
 
 # PTS grabs these flags in pts_test_installer.php
 export CFLAGS='-Wall'
@@ -27,6 +27,6 @@ host=`hostname`
 export TEST_RESULTS_IDENTIFIER=$host-intel-opt-id
 export TEST_RESULTS_NAME=$host-intel-opt-name
 export TEST_RESULTS_DESCRIPTION='A test suite'
-NO_FILE_HASH_CHECKS=1 PTS_SILENT_MODE=1 $PTS_BIN batch-benchmark pts-system
+DISPLAY=:0 NO_FILE_HASH_CHECKS=1 PTS_SILENT_MODE=1 $PTS_BIN batch-benchmark pts-system
 
 #$PTS_BIN result-file-raw-to-csv $TEST_RESULTS_NAME
