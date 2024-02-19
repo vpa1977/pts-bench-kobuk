0997698f6ca7eda1606304a5dd264c278d098d69
Vladimir Petko <vladimir.petko@canonical.com>
2024-02-20 11:38:12 +1300

    do not echo

--- g++.sh
+++ g++.sh
@@ -2,5 +2,4 @@
 args="$*"
 #args=`echo $args | sed 's/-fomit-frame-pointer//g'`
 #args="$args"
-echo /usr/bin/g++-13 $OMIT_POINTER $args
 /usr/bin/g++-13 $OMIT_POINTER $args
--- gcc.sh
+++ gcc.sh
@@ -2,5 +2,4 @@
 args="$*"
 #args=`echo $args | sed 's/-fomit-frame-pointer//g'`
 #args="$args"
-echo /usr/bin/gcc-13 $OMIT_POINTER $args
 /usr/bin/gcc-13 $OMIT_POINTER $args
