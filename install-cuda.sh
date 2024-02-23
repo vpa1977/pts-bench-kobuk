8b4d6a10bb2b2f910c8af91a03fa4eb53b07994d
Vladimir Petko <vladimir.petko@canonical.com>
2024-02-23 15:42:48 +1300

    add cuda

---
+++ install-cuda.sh
@@ -0,0 +1,3 @@
+wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
+sudo sh cuda_12.2.0_535.54.03_linux.run
+sudo ubuntu-drivers list --gpgpu
