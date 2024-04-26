60c52861292f5470eb0f60a6c2018ea493ee4794
Vladimir Petko <vladimir.petko@canonical.com>
2024-04-26 22:03:30 +1200

    missing

--- install-deps.sh
+++ install-deps.sh
@@ -34,7 +34,8 @@ sudo ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 /usr/lib/libOpenCL.so
 # system-pts dependencies
 sudo apt install -y libpcre3-dev redis libtiff-tools libjpeg-progs libxml2 libxml2-utils blender clpeak pbzip2 cryptsetup gegl gmic gnuradio hugin inkscape \
     rawtherapee libreoffice tesseract-ocr sqlite3 librsvg2-bin octave openscad ocrmypdf nginx mpv gimp \
-    lightdm dbus-x11 darktable flex libelf-dev libgmp3-dev libmpc-dev texinfo libmpfr-dev libisl-dev bison
+    lightdm dbus-x11 darktable flex libelf-dev libgmp3-dev libmpc-dev texinfo libmpfr-dev libisl-dev bison \
+    libcurl4-openssl-dev cargo
 
 # disable lightdm shudown
 sudo -u lightdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
